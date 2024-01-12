56ef327ead0aea586c05704e87f96b71232d74e6
ThiagoPizzirani <pizzthi@gmail.com>
2024-01-11 19:41:23 -0700

    first classes - jobs and resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Jobs job1 = new Jobs();
+        job1._jobTitle = "Database Specialist";
+        job1._company = "IBM";
+        job1._startYear = 2002;
+        job1._endYear = 2010;
+
+        Jobs job2 = new Jobs();
+        job2._jobTitle = "Quality Control";
+        job2._company = "Toyota";
+        job2._startYear = 1998;
+        job2._endYear = 2013;
+
+     //   job1.Display();
+     //   job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Thiago Pizzirani";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
