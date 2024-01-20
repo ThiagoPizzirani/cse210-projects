70ad3a15d932b754002af275ff589af8cb31ac71
ThiagoPizzirani <pizzthi@gmail.com>
2024-01-19 18:47:45 -0700

    fraction cs program

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,59 @@
+public class Fraction
+{
+
+    private int _top;
+    private int _bottom; 
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int SetTop(int top)
+    {
+        _top = top;
+        return _top;
+    }
+
+    public double GetBottom()
+    {
+        return _bottom;
+    }
+
+    public int SetBottom(int bottom)
+    {
+        _bottom = bottom;
+        return _bottom;
+    }
+
+    public void GetFractionString()
+    {
+        Console.WriteLine($"{GetTop()}/{GetBottom()}");
+    }
+
+    public void GetDecimalValue()
+    {
+        double fraction = GetTop()/GetBottom();
+        Console.WriteLine($"{fraction}");
+    }
+
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f = new Fraction();
+        f.GetFractionString();
+        f.GetDecimalValue();
+        
+        f.SetTop(5);
+        f.GetFractionString();
+        f.GetDecimalValue();
+
+        f.SetTop(3);
+        f.SetBottom(4);
+        f.GetFractionString();
+        f.GetDecimalValue();
+
+        f.SetTop(1);
+        f.SetBottom(3);
+        f.GetFractionString();
+        f.GetDecimalValue();
+
     }
 }
