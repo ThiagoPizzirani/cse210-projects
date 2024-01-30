ccd816c47bf149f7cc2b0f865b6f243db9c91d26
ThiagoPizzirani <pizzthi@gmail.com>
2024-01-29 20:49:08 -0700

    activity completed 1/29

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("James", "New Homework");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Roberto Rodrigues","Fractions","7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("The Cause of WW2", "Mary Waters", "European History");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWrittingInformation());
     }
 }
