555f596a380692d33395c85ed62e81edcc82bcd8
ThiagoPizzirani <pizzthi@gmail.com>
2024-02-05 19:13:16 -0700

    learning week 5 shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square quad = new Square(22.50, "Blue");
+        string color = quad.GetColor();
+        double area = quad.GetArea();
+
+        Console.WriteLine($"Square color is {color} and area of {area}");
+
+        Rectangle rect = new Rectangle(10, 15, "Red");
+        string colorRect = rect.GetColor();
+        double areaRect = rect.GetArea();
+
+        Console.WriteLine($"\nRectangle color is {colorRect} and area of {areaRect}");
+
+        Circle circ = new Circle(13.15, "Green");
+        string colorCirc = circ.GetColor();
+        double areaCirc = circ.GetArea();
+
+        Console.WriteLine($"\nCircle color is {colorCirc} and area of {areaCirc}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(quad);
+        shapes.Add(rect);
+        shapes.Add(circ);
+
+        foreach (Shape shape in shapes)
+        {
+            string colorShape = shape.GetColor();
+            double areaShape = shape.GetArea();
+
+            Console.WriteLine($"\nFrom the list, the shape color is {colorShape} and the area is {areaShape}");
+        }
+
+
     }
 }
